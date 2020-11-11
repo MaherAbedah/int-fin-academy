598bd9f341b36f23c1396715ad5de5c14df2e13b
Maher Abedah <m.abedah1@gmail.com>
2020-11-11 19:22:39 +0200

    Updates

---
+++ precache-manifest.55b16b1b8405e03a86d58cc327c174fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a859d3788a7998b01ddb844404499d4",
+    "url": "/int-fin-academy/index.html"
+  },
+  {
+    "revision": "3cd4bd176cbef3a3b26d",
+    "url": "/int-fin-academy/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "ec5a1b2d27286283cbac",
+    "url": "/int-fin-academy/static/js/2.c318a5a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/int-fin-academy/static/js/2.c318a5a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cd4bd176cbef3a3b26d",
+    "url": "/int-fin-academy/static/js/main.13393703.chunk.js"
+  },
+  {
+    "revision": "ca1982912925c466af1c",
+    "url": "/int-fin-academy/static/js/runtime-main.a18e3255.js"
+  }
+]);
